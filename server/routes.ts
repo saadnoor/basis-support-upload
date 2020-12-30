79c8c34188b946bf25ccc0e46fb4a55e7188f671
salehin <shwapneel>
2020-12-30 21:53:15 +0600

    Refactor: modulirized upload middleware for reusibility

--- server/routes.ts
+++ server/routes.ts
@@ -1,34 +1,8 @@
 import * as express from 'express';
-const multer  = require('multer');
-
-import * as AWS from 'aws-sdk';
-let multerS3 = require('multer-s3');
-
 import CatCtrl from './controllers/cat';
 import UserCtrl from './controllers/user';
 import FileCtrl from './controllers/file';
-
-const BUCKET_NAME = "basis-europe";
-const IAM_USER_KEY = "AKIA5CDIZQJKVE2TONPT";
-const IAM_USER_SECRET = "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";
-
-const s3bucket = new AWS.S3({
-  accessKeyId: IAM_USER_KEY,
-  secretAccessKey: IAM_USER_SECRET
-});
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3bucket,
-    bucket: BUCKET_NAME,
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
-      cb(null, `${Date.now().toString()}${file.originalname}`);    }
-  })
-})
+import upload from './middleware/file-upload';
 
 function setRoutes(app): void {
   const router = express.Router();
