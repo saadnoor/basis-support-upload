2ea0791bb069a46eab309fe84706f6308970a714
salehin <shwapneel>
2021-01-01 19:15:11 +0600

    Feat: Implemented mobile responsive design for login and registration page

--- client/app/services/customer-support.service.ts
+++ client/app/services/customer-support.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Cat } from '../shared/models/cat.model';
-
 @Injectable()
 export class CustomerSupportService {
 
--- client/app/shared/models/cat.model.ts
+++
@@ -1,7 +0,0 @@
-export class Cat {
-  // tslint:disable-next-line: variable-name
-  _id?: string;
-  name?: string;
-  weight?: number;
-  age?: number;
-}
